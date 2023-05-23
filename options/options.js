cd84d59c2b22469a352fa07228ea8bc43d350912
Crowdbotics <team@crowdbotics.com>
2023-05-23 08:05:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "zegocloud-live-stre-40917",
+  url: "https://zegocloud-live-stre-40917.botics.co",
+  api: "https://zegocloud-live-stre-40917.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
