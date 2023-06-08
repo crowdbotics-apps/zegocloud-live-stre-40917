db386aa3e78a9a499e87e2cd0f335ad53aecbec3
Shoaib Amjad <muhammad.shoaib@crowdbotics.com>
2023-06-08 12:20:19 +0500

    style changed

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView, Text } from "react-native";
 
 const Untitled2 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
+    <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
-    </SafeAreaView>;
+    }}>
+
+
+      <Text style={{color:'red', fontSize:50, fontWeight:'900',}}>
+        Testing screeen!!
+      </Text>
+    </ScrollView>
+  </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
